ad6162ecafa856de2d5f1a6b27cb7507f2285610
ggojlik <gabrierla.gojlik@gmail.com>
2026-09-22 17:50:24 +0200

    Aktualizacja projektu i wczytanie danych CSV

--- projekt semestralny.py
+++ projekt semestralny.py
@@ -1,1 +1,26 @@
-print ("hello")
+import pandas as pd
+from pathlib import Path
+
+# Lokalizacja projektu
+BASE_DIR = Path(__file__).resolve().parent
+
+# Lokalizacja pliku CSV
+file_path = BASE_DIR / "dane_syntetyczne_500_rekordow.csv"
+
+# Wczytanie danych
+df = pd.read_csv(
+    file_path,
+    sep=";",
+    decimal=","
+)
+
+print("Dane zostały poprawnie wczytane!")
+
+print("\nWymiary zbioru:")
+print(df.shape)
+
+print("\nPierwsze 5 rekordów:")
+print(df.head())
+
+print("\nInformacje o danych:")
+df.info()
